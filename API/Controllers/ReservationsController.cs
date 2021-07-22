45a1f9dff5b1a0c3e47499f7a39b40a8ea6409a7
Daniel Moura <danielmoura@MacBook-Pro.local>
2021-07-22 13:18:52 -0300

    Implementing mediator pattern

--- API/Controllers/ReservationsController.cs
+++ API/Controllers/ReservationsController.cs
@@ -1,30 +1,25 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Reservations;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class ReservationsController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ReservationsController(DataContext context)
-        {
-            _context = context;
-        }
 
         [HttpGet]
-        public async Task<ActionResult<List<Reservation>>> GetReservations() 
+        public async Task<ActionResult<List<Reservation>>> GetReservations()
         {
-            return await _context.Reservations.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Reservation>> GetReservation(Guid id){
-            return await _context.Reservations.FindAsync(id);
+        public async Task<ActionResult<Reservation>> GetReservation(Guid id)
+        {
+            return await Mediator.Send( new Details.Query{Id = id});
         }
     }
 }
---
+++ Application/Reservations/Details.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Reservations
+{
+    public class Details
+    {
+        public class Query : IRequest<Reservation>
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Reservation>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+
+            }
+            public async Task<Reservation> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _context.Reservations.FindAsync(request.Id);
+            }
+        }
+    }
+}
